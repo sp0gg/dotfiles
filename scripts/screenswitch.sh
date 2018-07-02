b49618c9c717f00a63c5c74f797a43b4566f5d99
Joe Nelson <jnelson@onshift.com>
2018-07-02 07:18:16 -0400

    adjust screen pos

--- scripts/screenswitch.sh
+++ scripts/screenswitch.sh
@@ -3,5 +3,5 @@
 xrandr --output DP1 --auto --primary
 xrandr --output DP2 --auto --right-of DP1
 #xrandr --output eDP1 --mode 1920x1080 --below DP1
-xrandr --output eDP1 --mode 1920x1080 --pos 960x1080
+xrandr --output eDP1 --mode 1920x1080 --pos 1080x1080
 i3-msg restart
