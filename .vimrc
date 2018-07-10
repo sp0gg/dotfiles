f554d82f719ee78d39c3e72779f5b58b9e4f28d4
Joe Nelson <jnelson@onshift.com>
2018-07-10 09:36:04 -0400

    CPU label, workspaces config, vim syntax

--- .vimrc
+++ .vimrc
@@ -2,3 +2,4 @@ set number
 set numberwidth=2
 highlight LineNr ctermfg=DarkGrey
 set shiftwidth=2
+syntax on
--- scripts/screenswitch.sh
+++ scripts/screenswitch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-xrandr --output DP1 --auto --primary
-xrandr --output DP2 --auto --right-of DP1
+xrandr --output DP2 --auto --primary
+xrandr --output DP1 --auto --right-of DP1
 #xrandr --output eDP1 --mode 1920x1080 --below DP1
 xrandr --output eDP1 --mode 1920x1080 --pos 1080x1080
 i3-msg restart
