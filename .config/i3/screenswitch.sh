178947c30a6d9040ee4513f83c80921ee549b68a
Joe Nelson <jnelson@onshift.com>
2018-10-02 10:13:55 -0400

    add screen layout command

---
+++ .config/i3/screenswitch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#xrandr --output DP2 --auto --primary
+#xrandr --output DP1 --auto --right-of DP1
+#xrandr --output eDP1 --mode 1920x1080 --pos 1080x1080
+#i3-msg restart
+
+bash ~/.config/i3/triple_monitor.sh
